19fcde751700ef788f958c72edf29447064a05a0
Philip <4144601+philipbrembeck@users.noreply.github.com>
2021-08-04 19:41:29 +0200

    Switched to Bootstrap `v5.1.0`

--- index.php
+++ index.php
@@ -10,38 +10,38 @@ if (file_exists('source/'.$CurPageURL.'.md')) {
   $Parsedown = new Parsedown();
 
   echo '<!DOCTYPE html>
-  <html lang="en">
-  <head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>JokeNetwork » '.$file.'</title>
-  <link href="https://faq.jokenetwork.de/css/bootstrap.css" rel="stylesheet">
-  <link href="https://faq.jokenetwork.de/css/fa/all.min.css" rel="stylesheet">
-  </head>
+<html lang="en">
 
-  <body>
-  <div class="container">
-    <div class="page-header">
-      <h1><a href="/">Docs</a> » '.$file.'</h1>
-      <p><a href="https://github.com/JokeNetwork/faq/blob/main/source/'.$file.'.md"><i class="fab fa-github"></i> Edit on GitHub</a></p>
-    </div>
-    <div class="content">
-    '.$Parsedown->text($content).'
-    </div>
-  <hr>  
-  <div class="footer">
-  <p class="pull-right">Licensed under <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/"><i class="fab fa-creative-commons"></i> C-BY-NC-SA 4.0</a></p>
-    <p>
-    <span itemscope itemtype="http://data-vocabulary.org/Breadcrumb"><a href="https://faq.jokenetwork.de" itemprop="url"><span itemprop="title">Documentations (Metatags)</span></a> ›</span>
+<head>
+    <meta charset="utf-8">
+    <title>MetaTag Docs » '.$file.'</title>
+    <meta name="description" content="Documentation for MetaTags in HTML">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/fa/all.min.css" rel="stylesheet">
+    <link href="css/addons.css" rel="stylesheet">
+</head>
 
-   <span itemscope itemtype="http://data-vocabulary.org/Breadcrumb"><a href="https://faq.jokenetwork.de/'.$file.'" itemprop="url"><span itemprop="title">'.$file.'</span></a></span>
-    </p>
+<body>
+    <div class="col-lg-8 mx-auto p-3 py-md-5">
+        <header class="border-bottom mb-4">
+            <h1><a href="/">Docs</a> » '.$file.'</h1>
+            <p><a href="https://github.com/JokeNetwork/faq/blob/main/source/'.$file.'.md"><i class="fab fa-github"></i> Edit on GitHub</a></p>
+        </header>
+        <main class="content">
+            '.$Parsedown->text($content).'
+        </main>
+        <footer class="pt-3 my-4 text-muted border-top fs-6">
+            <p class="float-start fs-6">
+                <span itemscope itemtype="http://data-vocabulary.org/Breadcrumb"><a href="https://faq.jokenetwork.de" itemprop="url"><span itemprop="title">Documentations (Metatags)</span></a> ›</span>
+                <span itemscope itemtype="http://data-vocabulary.org/Breadcrumb"><a href="https://faq.jokenetwork.de/'.$file.'" itemprop="url"><span itemprop="title">'.$file.'</span></a></span>
+            </p>
+            <p class="float-end fs-6">Licensed under <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/"><i class="fab fa-creative-commons"></i> C-BY-NC-SA 4.0</a></p>
+        </footer>
     </div>
-  </div>
     <script async src="https://analytics.jokenetwork.de/tracker.js" data-ackee-server="https://analytics.jokenetwork.de" data-ackee-domain-id="ad280e82-bc14-44a1-9758-e000a6c2f475"></script>
-  </body>
-  </html>';
+</body>
+
+</html>';
 
 } 
 
@@ -53,79 +53,76 @@ elseif (empty($CurPageURL)) {
   $Parsedown = new Parsedown();
 
   echo '<!DOCTYPE html>
-  <html lang="en">
-  <head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>JokeNetwork » Meta Tags</title>
-  <link href="https://faq.jokenetwork.de/css/bootstrap.css" rel="stylesheet">
-  <link href="https://faq.jokenetwork.de/css/fa/all.min.css" rel="stylesheet">
-  </head>
+<html lang="en">
 
-  <body>
-  <div class="container">
-    <div class="page-header">
-      <h1>Docs</h1>
-      <p><a href="https://github.com/JokeNetwork/faq/blob/main/source/'.$file.'.md"><i class="fab fa-github"></i> Edit on GitHub</a></p>
-    </div>
-    <div class="content">
-    '.$Parsedown->text($content).'
-    </div>
-  <hr> 
-  <div class="footer">
-  <p class="pull-right">Licensed under <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/"><i class="fab fa-creative-commons"></i> C-BY-NC-SA 4.0</a></p>
-    <p>
-    <span itemscope itemtype="http://data-vocabulary.org/Breadcrumb"><a href="https://faq.jokenetwork.de/'.$file.'" itemprop="url"><span itemprop="title">Documentations (Metatags)</span></a></span>
-    </p>
-    </div>
+<head>
+    <meta charset="utf-8">
+    <title>MetaTag Docs » index</title>
+    <meta name="description" content="Documentation for MetaTags in HTML">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/fa/all.min.css" rel="stylesheet">
+    <link href="css/addons.css" rel="stylesheet">
+</head>
+
+<body>
+    <div class="col-lg-8 mx-auto p-3 py-md-5">
+        <header class="border-bottom mb-4">
+            <h1>Docs</h1>
+            <p><a href="https://github.com/JokeNetwork/faq/blob/main/source/'.$file.'.md"><i class="fab fa-github"></i> Edit on GitHub</a></p>
+        </header>
+        <main class="content">
+            '.$Parsedown->text($content).'
+        </main>
+        <footer class="pt-3 my-4 text-muted border-top fs-6">
+            <p class="float-start fs-6">
+                <span itemscope itemtype="http://data-vocabulary.org/Breadcrumb"><a href="https://faq.jokenetwork.de" itemprop="url"><span itemprop="title">Documentations (Metatags)</span></a></span>
+            </p>
+            <p class="float-end fs-6">Licensed under <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/"><i class="fab fa-creative-commons"></i> C-BY-NC-SA 4.0</a></p>
+        </footer>
     </div>
     <script async src="https://analytics.jokenetwork.de/tracker.js" data-ackee-server="https://analytics.jokenetwork.de" data-ackee-domain-id="ad280e82-bc14-44a1-9758-e000a6c2f475"></script>
-  </body>
-  </html>';
+</body>
+
+</html>';
 }
 
 else {
-  if($_GET['code'] == "404"){$error = "No such file";}
-  elseif ($_GET['code'] == "403"){$error = "Access failed";}
+  if($_GET['code'] == "404"){$error = "No such file"; header("HTTP/1.1 404 Not found");}
+  elseif ($_GET['code'] == "403"){$error = "Access failed"; header("HTTP/1.1 403 Access Denied");}
   else {$error = "Error";}
   echo '<!DOCTYPE html>
-  <html lang="en">
-    <head>
-      <meta charset="utf-8">
-      <meta http-equiv="X-UA-Compatible" content="IE=edge">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>JokeNetwork » Error '.$_GET['code'].'</title>
-      <link href="https://faq.jokenetwork.de/css/bootstrap.css" rel="stylesheet">
-      <link href="https://faq.jokenetwork.de/css/fa/all.min.css" rel="stylesheet">
-    </head>
+<html lang="en">
 
-    <body>
-      <div class="container">
+<head>
+    <meta charset="utf-8">
+    <title>MetaTag Docs » Error '.$error.'</title>
+    <meta name="description" content="Documentation for MetaTags in HTML">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/fa/all.min.css" rel="stylesheet">
+    <link href="css/addons.css" rel="stylesheet">
+</head>
 
-        <div class="page-header">
-          <h1><a href="/">Docs</a> » Error Code '.$_GET['code'].'</h1>
-          <p class="lead">Server returned error code '.$_GET['code'].' for '.$CurPageURL.'</p>
-        </div>
-        <div class="content">
-        <h3>'.$CurPageURL.': '.$error.'</h3>
-        Go to <a href="https://faq.jokenetwork.de/index">index</a> for an overview.
-        </div>
-        <hr>
-  <div class="footer">
-  <p class="pull-right">Licensed under <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/"><i class="fab fa-creative-commons"></i> C-BY-NC-SA 4.0</a></p>
+<body>
+    <div class="col-lg-8 mx-auto p-3 py-md-5">
+        <header class="border-bottom mb-4">
+            <h1><a href="/">Docs</a> » Error code '.$error.'</h1>
+        </header>
+        <main class="content">
+            <h3>'.$CurPageURL.': '.$error.'</h3>
+            Go to <a href="index">index</a> for an overview.
+        </main>
+        <footer class="pt-3 my-4 text-muted border-top fs-6">
+            <p class="float-start fs-6">
+                <span itemscope itemtype="http://data-vocabulary.org/Breadcrumb"><a href="https://faq.jokenetwork.de" itemprop="url"><span itemprop="title">Documentations (Metatags)</span></a> ›</span>
+                <span itemscope itemtype="http://data-vocabulary.org/Breadcrumb"><a href="https://faq.jokenetwork.de/<?php echo $file ?>" itemprop="url"><span itemprop="title">'.$CurPageURL.'</span></a></span>
+            </p>
+            <p class="float-end fs-6">Licensed under <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/"><i class="fab fa-creative-commons"></i> C-BY-NC-SA 4.0</a></p>
+        </footer>
+    </div>
+    <script async src="https://analytics.jokenetwork.de/tracker.js" data-ackee-server="https://analytics.jokenetwork.de" data-ackee-domain-id="ad280e82-bc14-44a1-9758-e000a6c2f475"></script>
+</body>
 
-    <p>
-    <span itemscope itemtype="http://data-vocabulary.org/Breadcrumb"><a href="https://faq.jokenetwork.de" itemprop="url"><span itemprop="title">Documentations (Metatags)</span></a> ›</span>
-
-   <span itemscope itemtype="http://data-vocabulary.org/Breadcrumb"><a href="https://faq.jokenetwork.de/<?php echo $file ?>" itemprop="url"><span itemprop="title">'.$CurPageURL.'</span></a></span>
-    </p>
-   </div>
-   </div>
-
-     <script async src="https://analytics.jokenetwork.de/tracker.js" data-ackee-server="https://analytics.jokenetwork.de" data-ackee-domain-id="ad280e82-bc14-44a1-9758-e000a6c2f475"></script>
-    </body>
-  </html>';
+</html>';
 }
 
 ?>
