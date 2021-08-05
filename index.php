8b59d685b1dcd785e032dd840f29efb382d21d37
Philip <4144601+philipbrembeck@users.noreply.github.com>
2021-08-05 14:11:12 +0200

    Added Darkmode switch

--- index.php
+++ index.php
@@ -9,6 +9,8 @@ if (file_get_contents('https://raw.githubusercontent.com/JokeNetwork/faq/main/so
   $content = file_get_contents('https://raw.githubusercontent.com/JokeNetwork/faq/main/source/'.$file.'.md');
   $Parsedown = new Parsedown();
 
+  $fileshort = strlen($file) > 5 ? substr($file,0,5)."..." : $file;
+
   echo '<!DOCTYPE html>
 <html lang="en">
 
@@ -19,25 +21,40 @@ if (file_get_contents('https://raw.githubusercontent.com/JokeNetwork/faq/main/so
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="css/fa/all.min.css" rel="stylesheet">
     <link href="css/addons.css" rel="stylesheet">
+    <link href="css/darkmode.css" rel="stylesheet">
 </head>
 
-<body>
+<body data-theme="dark">
     <div class="col-lg-8 mx-auto p-3 py-md-5">
-        <header class="border-bottom mb-4">
-            <h1><a href="/">Docs</a> » '.$file.'</h1>
-            <p><a href="https://github.com/JokeNetwork/faq/blob/main/source/'.$file.'.md"><i class="fab fa-github"></i> Edit on GitHub</a></p>
-        </header>
-        <main class="content">
+    <header class="mb-5">
+      <h1 class="float-md-start"><a href="/">Docs</a> » '.$fileshort.'</h1>
+      <nav class="nav justify-content-center float-md-end">
+        <a class="nav-link" href="https://github.com/JokeNetwork/faq/blob/main/source/'.$file.'.md"><i class="fab fa-github"></i> Edit on GitHub</a>
+        <div class="nav-link">
+          <div class="form-check form-switch">
+            <input type="checkbox" class="form-check-input" id="darkSwitch">
+            <label class="custom-control-label" id="darkmode" for="darkSwitch"></label>
+          </div>
+        </div>
+      </nav>
+      <div style="clear:both"></div>
+    </header>
+    <main class="content">
             '.$Parsedown->text($content).'
         </main>
         <footer class="pt-3 my-4 text-muted border-top fs-6">
             <p class="float-start fs-6">
                 <span itemscope itemtype="http://data-vocabulary.org/Breadcrumb"><a href="https://faq.jokenetwork.de" itemprop="url"><span itemprop="title">Documentations (Metatags)</span></a> ›</span>
-                <span itemscope itemtype="http://data-vocabulary.org/Breadcrumb"><a href="https://faq.jokenetwork.de/'.$file.'" itemprop="url"><span itemprop="title">'.$file.'</span></a></span>
+                <span itemscope itemtype="http://data-vocabulary.org/Breadcrumb"><a href="https://faq.jokenetwork.de/'.$file.'" itemprop="url"><span itemprop="title">'.$fileshort.'</span></a></span>
             </p>
             <p class="float-end fs-6">Licensed under <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/"><i class="fab fa-creative-commons"></i> C-BY-NC-SA 4.0</a></p>
+
+            <div class="sponsor">
+             <a href="//github.com/sponsors/philipbrembeck/" class="btn-dark" role="button"><i class="far fa-heart"></i> Consider Sponsoring</a>
+            </div>
         </footer>
     </div>
+    <script src="js/dark-mode-switch.min.js"></script>
     <script async src="https://analytics.jokenetwork.de/tracker.js" data-ackee-server="https://analytics.jokenetwork.de" data-ackee-domain-id="ad280e82-bc14-44a1-9758-e000a6c2f475"></script>
 </body>
 
@@ -52,6 +69,8 @@ elseif (empty($CurPageURL)) {
   $content = file_get_contents('https://raw.githubusercontent.com/JokeNetwork/faq/main/source/'.$file.'.md');
   $Parsedown = new Parsedown();
 
+  $fileshort = strlen($file) > 5 ? substr($file,0,5)."..." : $file;
+
   echo '<!DOCTYPE html>
 <html lang="en">
 
@@ -62,15 +81,25 @@ elseif (empty($CurPageURL)) {
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="css/fa/all.min.css" rel="stylesheet">
     <link href="css/addons.css" rel="stylesheet">
+    <link href="css/darkmode.css" rel="stylesheet">
 </head>
 
-<body>
+<body data-theme="dark">
     <div class="col-lg-8 mx-auto p-3 py-md-5">
-        <header class="border-bottom mb-4">
-            <h1>Docs</h1>
-            <p><a href="https://github.com/JokeNetwork/faq/blob/main/source/'.$file.'.md"><i class="fab fa-github"></i> Edit on GitHub</a></p>
-        </header>
-        <main class="content">
+    <header class="mb-5">
+      <h1 class="float-md-start">Docs</h1>
+      <nav class="nav justify-content-center float-md-end">
+        <a class="nav-link" href="https://github.com/JokeNetwork/faq/blob/main/source/'.$file.'.md"><i class="fab fa-github"></i> Edit on GitHub</a>
+        <div class="nav-link">
+          <div class="form-check form-switch">
+            <input type="checkbox" class="form-check-input" id="darkSwitch">
+            <label class="custom-control-label" id="darkmode" for="darkSwitch"></label>
+          </div>
+        </div>
+      </nav>
+      <div style="clear:both"></div>
+    </header>
+    <main class="content">
             '.$Parsedown->text($content);
 
             function mkmap($dir){
@@ -94,8 +123,13 @@ elseif (empty($CurPageURL)) {
                 <span itemscope itemtype="http://data-vocabulary.org/Breadcrumb"><a href="https://faq.jokenetwork.de" itemprop="url"><span itemprop="title">Documentations (Metatags)</span></a></span>
             </p>
             <p class="float-end fs-6">Licensed under <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/"><i class="fab fa-creative-commons"></i> C-BY-NC-SA 4.0</a></p>
+
+            <div class="sponsor">
+             <a href="//github.com/sponsors/philipbrembeck/" class="btn-dark" role="button"><i class="far fa-heart"></i> Consider Sponsoring</a>
+            </div>
         </footer>
     </div>
+    <script src="js/dark-mode-switch.min.js"></script>
     <script async src="https://analytics.jokenetwork.de/tracker.js" data-ackee-server="https://analytics.jokenetwork.de" data-ackee-domain-id="ad280e82-bc14-44a1-9758-e000a6c2f475"></script>
 </body>
 
@@ -106,35 +140,55 @@ elseif (!empty($CurPageURL) && file_get_contents('https://raw.githubusercontent.
   if($_GET['code'] == "404"){$error = "No such file"; header("HTTP/1.1 404 Not found");}
   elseif ($_GET['code'] == "403"){$error = "Access failed"; header("HTTP/1.1 403 Access Denied");}
   else {$error = "Error";}
+
+  $fileshort = strlen($CurPageURL) > 5 ? substr($CurPageURL,0,5)."..." : $$CurPageURL;
+  $filemid = strlen($CurPageURL) > 5 ? substr($CurPageURL,0,15)."..." : $$CurPageURL;
+
   echo '<!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="utf-8">
-    <title>MetaTag Docs » Error '.$error.'</title>
+    <title>MetaTag Docs » '.$error.'</title>
     <meta name="description" content="Documentation for MetaTags in HTML">
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="css/fa/all.min.css" rel="stylesheet">
     <link href="css/addons.css" rel="stylesheet">
+    <link href="css/darkmode.css" rel="stylesheet">
 </head>
 
-<body>
+<body data-theme="dark">
     <div class="col-lg-8 mx-auto p-3 py-md-5">
-        <header class="border-bottom mb-4">
-            <h1><a href="/">Docs</a> » Error code '.$error.'</h1>
-        </header>
-        <main class="content">
-            <h3>'.$CurPageURL.': '.$error.'</h3>
+    <header class="mb-5">
+      <h1 class="float-md-start"><a href="/">Docs</a> » Error code '.$_GET['code'].'</h1>
+      <nav class="nav justify-content-center float-md-end">
+        <a class="nav-link" href="https://github.com/JokeNetwork/faq/blob/main/source/"><i class="fab fa-github"></i> Create on GitHub</a>
+        <div class="nav-link">
+          <div class="form-check form-switch">
+            <input type="checkbox" class="form-check-input" id="darkSwitch">
+            <label class="custom-control-label" id="darkmode" for="darkSwitch"></label>
+          </div>
+        </div>
+      </nav>
+      <div style="clear:both"></div>
+    </header>
+    <main class="content">
+            <h3>'.$filemid.': '.$error.'</h3>
             Go to <a href="index">index</a> for an overview.
         </main>
         <footer class="pt-3 my-4 text-muted border-top fs-6">
             <p class="float-start fs-6">
                 <span itemscope itemtype="http://data-vocabulary.org/Breadcrumb"><a href="https://faq.jokenetwork.de" itemprop="url"><span itemprop="title">Documentations (Metatags)</span></a> ›</span>
-                <span itemscope itemtype="http://data-vocabulary.org/Breadcrumb"><a href="https://faq.jokenetwork.de/<?php echo $file ?>" itemprop="url"><span itemprop="title">'.$CurPageURL.'</span></a></span>
+                <span itemscope itemtype="http://data-vocabulary.org/Breadcrumb"><a href="https://faq.jokenetwork.de/'.$CurPageURL.'" itemprop="url"><span itemprop="title">'.$fileshort.'</span></a></span>
             </p>
             <p class="float-end fs-6">Licensed under <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/"><i class="fab fa-creative-commons"></i> C-BY-NC-SA 4.0</a></p>
+
+            <div class="sponsor">
+             <a href="//github.com/sponsors/philipbrembeck/" class="btn-dark" role="button"><i class="far fa-heart"></i> Consider Sponsoring</a>
+            </div>
         </footer>
     </div>
+    <script src="js/dark-mode-switch.min.js"></script>
     <script async src="https://analytics.jokenetwork.de/tracker.js" data-ackee-server="https://analytics.jokenetwork.de" data-ackee-domain-id="ad280e82-bc14-44a1-9758-e000a6c2f475"></script>
 </body>
 
