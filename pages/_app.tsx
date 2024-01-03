43a2e486f487208c84d8702484fb11a38486f5da
Philip <4144601+philipbrembeck@users.noreply.github.com>
2024-01-04 00:53:35 +0100

    fix(Build): Changed path

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import "styles/globals.scss";
+import "../styles/globals.scss";
  
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
